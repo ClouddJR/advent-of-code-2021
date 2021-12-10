c5a2bdec677f2fe57dfd353779ac351c074548af
Arkadiusz Chmura <arekchmura@gmail.com>
2021-12-10 09:38:30 +0100

    Add solution for day 10

---
+++ src/main/kotlin/com/clouddjr/advent2021/Day10.kt
@@ -0,0 +1,58 @@
+package com.clouddjr.advent2021
+
+class Day10(private val input: List<String>) {
+
+    private val matching = mapOf(
+        '(' to ')',
+        '[' to ']',
+        '{' to '}',
+        '<' to '>'
+    )
+    private val scoresIllegal = mapOf(
+        ')' to 3,
+        ']' to 57,
+        '}' to 1197,
+        '>' to 25137,
+    )
+    private val scoresCompletion = mapOf(
+        ')' to 1,
+        ']' to 2,
+        '}' to 3,
+        '>' to 4,
+    )
+
+    fun solvePart1(): Int {
+        return input
+            .sumOf { line ->
+                with(ArrayDeque<Char>()) {
+                    line.sumOf { c ->
+                        when {
+                            c in matching.keys -> (0).also { addLast(c) }
+                            matching[removeLast()] != c -> scoresIllegal.getValue(c)
+                            else -> 0
+                        }
+                    }
+                }
+            }
+    }
+
+    fun solvePart2(): Long {
+        return input
+            .map { line ->
+                with(ArrayDeque<Char>()) {
+                    line.forEach { c ->
+                        when {
+                            c in matching.keys -> addLast(c)
+                            matching[removeLast()] != c -> return@map null
+                        }
+                    }
+                    reversed()
+                        .map { matching.getValue(it) }
+                        .fold(0L) { acc, c -> (acc * 5 + scoresCompletion.getValue(c)) }
+                }
+            }
+            .filterNotNull()
+            .sorted()
+            .let { it[it.size / 2] }
+    }
+}
---
+++ src/test/kotlin/com/clouddjr/advent2021/Day10Test.kt
@@ -0,0 +1,60 @@
+package com.clouddjr.advent2021
+
+import com.clouddjr.advent2021.Resources.resourceAsList
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+@DisplayName("Day 10")
+class Day10Test {
+
+    private val input = """
+        [({(<(())[]>[[{[]{<()<>>
+        [(()[<>])]({[<{<<[]>>(
+        {([(<{}[<>[]}>{[]{[(<()>
+        (((({<>}<{<{<>}{[]{[]{}
+        [[<[([]))<([[{}[[()]]]
+        [{[{({}]{}}([{[{{{}}([]
+        {<[[]]>}<{[{[{[]{()[[[]
+        [<(<(<(<{}))><([]([]()
+        <{([([[(<>()){}]>(<<{{
+        <{([{{}}[<[[[<>{}]]]>[]]
+    """.trimIndent().lines()
+
+    @Nested
+    @DisplayName("Part 1")
+    inner class Part1 {
+        @Test
+        fun `Matches example`() {
+            val answer = Day10(input).solvePart1()
+
+            assertEquals(26_397, answer)
+        }
+
+        @Test
+        fun `Matches actual`() {
+            val answer = Day10(resourceAsList("day10.txt")).solvePart1()
+
+            assertEquals(392_367, answer)
+        }
+    }
+
+    @Nested
+    @DisplayName("Part 2")
+    inner class Part2 {
+        @Test
+        fun `Matches example`() {
+            val answer = Day10(input).solvePart2()
+
+            assertEquals(288_957, answer)
+        }
+
+        @Test
+        fun `Matches actual`() {
+            val answer = Day10(resourceAsList("day10.txt")).solvePart2()
+
+            assertEquals(2_192_104_158, answer)
+        }
+    }
+}
