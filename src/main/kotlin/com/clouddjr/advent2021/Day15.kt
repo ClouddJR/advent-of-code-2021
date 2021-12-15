a9c5c77f637974f60b70dcce99169e45a9cd4a23
Arkadiusz Chmura <arekchmura@gmail.com>
2021-12-15 10:20:22 +0100

    Cleanup some code

--- src/main/kotlin/com/clouddjr/advent2021/Day15.kt
+++ src/main/kotlin/com/clouddjr/advent2021/Day15.kt
@@ -12,22 +12,22 @@ class Day15(input: List<String>) {
 
     fun solvePart2() = lowestRiskPath(expandedRiskLevels)
 
-    private fun lowestRiskPath(levels: List<List<Int>>): Int {
-        val dist = Array(levels.size) { Array(levels.first().size) { Int.MAX_VALUE } }.apply { get(0)[0] = 0 }
+    private fun lowestRiskPath(riskLevels: List<List<Int>>): Int {
+        val dist = Array(riskLevels.size) { Array(riskLevels.first().size) { Int.MAX_VALUE } }.apply { get(0)[0] = 0 }
 
         val toVisit = PriorityQueue<Pair<Int, Int>> { (pY, pX), (rY, rX) -> dist[pY][pX].compareTo(dist[rY][rX]) }
         val visited = mutableSetOf(0 to 0)
-
         toVisit.add(0 to 0)
+
         while (toVisit.isNotEmpty()) {
-            val (currentRow, currentCol) = toVisit.poll().also { visited.add(it) }
-            neighbours(currentRow, currentCol, levels).forEach { (nextRow, nextCol) ->
-                if (!visited.contains(nextRow to nextCol)) {
-                    (dist[currentRow][currentCol] + levels[nextRow][nextCol]).let { newDistance ->
-                        if (newDistance < dist[nextRow][nextCol]) {
-                            dist[nextRow][nextCol] = newDistance
-                            toVisit.offer(nextRow to nextCol)
-                        }
+            val (y, x) = toVisit.poll().also { visited.add(it) }
+
+            neighbours(y, x, riskLevels).forEach { (nY, nX) ->
+                if (!visited.contains(nY to nX)) {
+                    val newDistance = dist[y][x] + riskLevels[nY][nX]
+                    if (newDistance < dist[nY][nX]) {
+                        dist[nY][nX] = newDistance
+                        toVisit.add(nY to nX)
                     }
                 }
             }
@@ -45,9 +45,9 @@ class Day15(input: List<String>) {
 
     private fun List<Int>.increasedAndCapped(by: Int) = map { level -> (level + by).let { if (it > 9) it - 9 else it } }
 
-    private fun neighbours(row: Int, col: Int, levels: List<List<Int>>): List<Pair<Int, Int>> {
+    private fun neighbours(y: Int, x: Int, riskLevels: List<List<Int>>): List<Pair<Int, Int>> {
         return arrayOf((-1 to 0), (1 to 0), (0 to -1), (0 to 1))
-            .map { (dx, dy) -> row + dx to col + dy }
-            .filter { (x, y) -> x in levels.indices && y in levels.first().indices }
+            .map { (dy, dx) -> y + dy to x + dx }
+            .filter { (y, x) -> y in riskLevels.indices && x in riskLevels.first().indices }
     }
 }
